1f05640497b5204fa32d8cd89ab2cdee6877c697
Justin Bantuelle <justin@mudbugmedia.com>
2013-10-18 19:11:04 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beertap::Application.config.secret_key_base = '8ae5db6701cc1247690eeef89e61a1c544727ca25d25beb80e3f27bbe1247d1c68120a9840ea8fa1de54c43e58ca228f480f0c40e2273c21efb5d21b712e71b0'
