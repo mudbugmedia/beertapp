e319209a041e327c0c33bb2622c68b534846634c
Tuan Vu <tuan@mudbugmedia.com>
2013-10-19 13:54:50 -0500

    Guard against invalid beer input when creating a new profile.

--- app/controllers/beer_profiles_controller.rb
+++ app/controllers/beer_profiles_controller.rb
@@ -16,9 +16,13 @@ class BeerProfilesController < ApplicationController
 
   # GET /beer_profiles/new
   def new
-    @beer_profile = BeerProfile.new
-    @beer_profile.user_id = current_user.id
-    @beer_profile.beer_id = params["beer_id"]
+    if params["beer_id"].nil? or Beer.where(:id => params["beer_id"]).count == 0
+      redirect_to :action => :index
+    else
+      @beer_profile = BeerProfile.new
+      @beer_profile.user_id = current_user.id
+      @beer_profile.beer_id = params["beer_id"]
+    end
   end
 
   # GET /beer_profiles/1/edit
