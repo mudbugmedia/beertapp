0c690421078e9b8c92c8d89cec07a51cd20fea54
Tuan Vu <tuan@mudbugmedia.com>
2013-10-18 22:49:34 -0500

    Add guest user sign on.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_any_user
+    unless session[:guest_user_id]
+      authenticate_user!
+    end
+  end
+
+  def current_user
+    super || guest_user
+  end
+
+  private
+
+  def guest_user
+    User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+  end
+
+  def create_guest_user
+    u = User.create(:email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u.save(:validate => false)
+    u
+  end
+
 end
