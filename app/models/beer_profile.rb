352aa5ca0b69aea4532171d75545bf24ffbf4a8b
Justin Bantuelle <justin@mudbugmedia.com>
2013-10-19 12:57:11 -0500

    Rename Esthers to Esters

--- app/models/beer_profile.rb
+++ app/models/beer_profile.rb
@@ -1,6 +1,6 @@
 class BeerProfile < ActiveRecord::Base
 
   def flavors
-    [flavor_esthers, flavor_alcoholic, flavor_citrus, flavor_hoppy, flavor_floral, flavor_spicy, flavor_malty, flavor_toffee, flavor_burnt, flavor_sweet, flavor_sour, flavor_bitter, flavor_astringent, flavor_full_bodied, flavor_lingers]
+    [flavor_esters, flavor_alcoholic, flavor_citrus, flavor_hoppy, flavor_floral, flavor_spicy, flavor_malty, flavor_toffee, flavor_burnt, flavor_sweet, flavor_sour, flavor_bitter, flavor_astringent, flavor_full_bodied, flavor_lingers]
   end
 end
