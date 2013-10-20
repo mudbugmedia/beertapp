98225fa475f7e1ec9b77929f7f172fd156fe46d6
Justin Bantuelle <justin@mudbugmedia.com>
2013-10-20 15:00:47 -0500

    Hide Logo On Specific Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def hide_logo?(params)
+    sign_in_page? || homepage?
+  end
+
+  def sign_in_page?
+    params['controller'] == 'devise/sessions' && params['action'] == 'new'
+  end
+
+  def homepage?
+    params['controller'] == 'beers' && params['action'] == 'search'
+  end
 end
