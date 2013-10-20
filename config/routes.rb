380ad3777472a3c40b05b1138d2ff1215ea17f4b
Tuan Vu <tuan@mudbugmedia.com>
2013-10-20 12:15:30 -0500

    Delete duplicate root routes.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Beertap::Application.routes.draw do
   resources :beer_profiles
   root 'beers#search'
   get 'beer_profiles/users/:id' => 'beer_profiles#users'
-  root 'beer_profiles#index'
 
   post "user/guest_sign_in"
   devise_for :users
