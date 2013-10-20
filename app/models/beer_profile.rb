a22815d7e54c9811a9c2801225aa8e74f4f9525c
Justin Bantuelle <justin@mudbugmedia.com>
2013-10-19 19:15:40 -0500

    Add Associations on Models

--- app/models/beer_profile.rb
+++ app/models/beer_profile.rb
@@ -1,4 +1,6 @@
 class BeerProfile < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :beer
 
   def flavors
     [flavor_esters, flavor_alcoholic, flavor_citrus, flavor_hoppy, flavor_floral, flavor_spicy, flavor_malty, flavor_toffee, flavor_burnt, flavor_sweet, flavor_sour, flavor_bitter, flavor_astringent, flavor_full_bodied, flavor_lingers]
